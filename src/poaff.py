e35ff033dd42e457aadd4c17a3382f735810050d
BPascal-91 <pascal_bazile@yahoo.fr>
2020-06-07 19:54:43 +0200

    v1.2.0 - Forced reload by second phase (when necessary)

--- src/poaff.py
+++ src/poaff.py
@@ -25,9 +25,9 @@ appVersion          = bpaTools.getVersionFile()
 appId               = appName + " v" + appVersion
 outPath             = appPath + "../output/"
 logFile             = outPath + "_" + appName + ".log"
-bpaTools.createFolder(outPath)                              #Init dossier de sortie
+bpaTools.createFolder(outPath)                                      #Init dossier de sortie
 
-callingContext      = "Paragliding-OpenAir-FrenchFiles"         #Your app calling context
+callingContext      = "Paragliding-OpenAir-FrenchFiles"             #Your app calling context
 linkContext         = "http://pascal.bazile.free.fr/paraglidingFolder/divers/GPS/OpenAir-Format/"
 
 
@@ -41,8 +41,8 @@ scriptProcessing = {
     "BPa-ZonesComp":    {cstOutPath:"../output/BPa/", cstSrcFile:"../input/BPa/20191210_BPa_ZonesComplementaires_aixm45.xml"},
     "FFVP-Parcs":       {cstOutPath:"../output/FFVP/", cstSrcFile:"../input/FFVP/20191214_FFVP_ParcsNat_aixm45.xml"},
     "FFVP-Birds":       {cstOutPath:"../output/FFVP/", cstSrcFile:"../input/FFVP/20191214_FFVP_BirdsProtect_aixm45.xml"},
-    "SIA":              {cstOutPath:"../output/SIA/", cstSrcFile:"../input/SIA/20200618_aixm4.5_SIA-FR.xml"},
-    "EuCtrl":           {cstOutPath:"../output/EuCtrl/", cstSrcFile:"../input/EuCtrl/20200326_aixm4.5_Eurocontrol-FR.xml"}
+    #"SIA":              {cstOutPath:"../output/SIA/", cstSrcFile:"../input/SIA/20200618_aixm4.5_SIA-FR.xml"},
+    #"EuCtrl":           {cstOutPath:"../output/EuCtrl/", cstSrcFile:"../input/EuCtrl/20200326_aixm4.5_Eurocontrol-FR.xml"}
 }
     
 
@@ -52,14 +52,29 @@ aArgs = [appName, "-Fall", aixmReader.CONST.typeAIRSPACES, aixmReader.CONST.optF
 
 
 def parseFile(sKey:str, oFile:dict) -> bool:
-    bpaTools.createFolder(oFile["outPath"])     #Init dossier de sortie
+    bpaTools.createFolder(oFile["outPath"])                         #Init dossier de sortie
     aixmCtrl = aixmReader.AixmControler(oFile["srcFile"], oFile["outPath"], sKey, oLog)     #Init controler
-    ret = aixmCtrl.execParser(oOpts)            #Execution des traitements
+    ret = aixmCtrl.execParser(oOpts)                                #Execution des traitements
     return ret
 
 def updateReferentials(sKey:str, oFile:dict) -> None:
     oGEH = GroundEstimatedHeight(oLog, oFile["outPath"], oFile["outPath"] + "referentials/", sKey + "@")
-    oGEH.parseUnknownGroundHeightRef()          #Execution des traitements
+    oGEH.parseUnknownGroundHeightRef()                              #Execution des traitements
+    return
+
+def poaffGenerateFiles(sMsg:str=None) -> None:
+    if sMsg!=None:
+        oLog.warning("{0}".format(sMsg), outConsole=True)
+    oLog.writeCommandLine(aArgs)                                    #Trace le contexte d'execution
+    oLog.info("Loading module - {0}".format(aixmParserId), outConsole=True)
+    
+    #### Traitement des fichiers  ####
+    for sKey, oFile in scriptProcessing.items():
+        iPrevErr = oLog.CptCritical + oLog.CptError
+        parseFile(sKey, oFile)
+        iActErr = oLog.CptCritical + oLog.CptError
+        if iActErr!=iPrevErr:
+            updateReferentials(sKey, oFile)    
     return
 
 
@@ -67,20 +82,19 @@ if __name__ == '__main__':
     ####  Préparation d'appel ####
     oOpts = bpaTools.getCommandLineOptions(aArgs)                   #Arguments en dictionnaire
     oLog = bpaTools.Logger(appId, logFile, callingContext, linkContext, isSilent=bool(aixmReader.CONST.optSilent in oOpts))
-    
     if aixmReader.CONST.optCleanLog in oOpts:
-        oLog.resetFile()                                #Clean du log si demandé
-    oLog.writeCommandLine(aArgs)                        #Trace le contexte d'execution
-    oLog.info("Loading module - {}".format(aixmParserId), outConsole=True)
-    
-    #### Traitement des fichiers  ####
-    for sKey, oFile in scriptProcessing.items():
-        parseFile(sKey, oFile)
-        """if oLog.CptCritical or oLog.CptError:
-            break"""
-        updateReferentials(sKey, oFile)
+        oLog.resetFile()                                            #Clean du log si demandé
+
+    sCallingContext = None
+    poaffGenerateFiles()
+    if (oLog.CptCritical + oLog.CptError) > 0:
+        oLog.resetFile()                                            #Clean du log
+        sCallingContext = "Forced reload by second phase"
+        poaffGenerateFiles(sCallingContext)
 
     print()
-    oLog.Report()    
+    if sCallingContext!=None:
+        oLog.warning("{0}".format(sCallingContext), outConsole=True)
+    oLog.Report()
     oLog.closeFile()
 
