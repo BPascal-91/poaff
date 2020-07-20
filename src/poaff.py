51d69591a3c39bf679abd0a1bbc4d57b0ab69266
BPascal-91 <pascal_bazile@yahoo.fr>
2020-07-20 19:12:50 +0200

    v1.3.3

--- src/poaff.py
+++ src/poaff.py
@@ -23,6 +23,9 @@ appVersion          = bpaTools.getVersionFile()
 appId               = appName + " v" + appVersion
 outPath             = appPath + "../output/"
 logFile             = outPath + "_" + appName + ".log"
+globalHeader        = "global"
+referentialPath     = "referentials/"
+poaffOutPath        = outPath + "_POAFF/"
 poaffWebPath        = outPath + "_POAFF_www/"
 cfdWebPath          = outPath + "_CFD_www/"
 bpaTools.createFolder(outPath)                                      #Init dossier de sortie
@@ -30,6 +33,21 @@ bpaTools.createFolder(outPath)                                      #Init dossie
 callingContext      = "Paragliding-OpenAir-FrenchFiles"             #Your app calling context
 linkContext         = "http://pascal.bazile.free.fr/paraglidingFolder/divers/GPS/OpenAir-Format/"
 
+
+####  Constantes de paramétrage des catalogues  ####
+cstDeduplicateSeparator = "@@-"
+cstKeyCatalogType = "type"
+cstKeyCatalogHeaderFile = "headerFile"
+cstKeyCatalogCatalog = "catalog"
+cstKeyCatalogSoftware = "software"
+cstKeyCatalogCreated = "created"
+cstKeyCatalogContent = "content"
+cstKeyCatalogSrcFiles = "srcFiles"
+cstKeyCatalogSrcAixmFile = "srcAixmFile"
+cstKeyCatalogSrcAixmOrigin = "srcAixmOrigin"
+cstKeyCatalogSrcAixmVersion = "srcAixmVersion"
+cstKeyCatalogSrcAixmCreated = "srcAixmCreated"
+cstKeyCatalogOrgFile = "orgFile"
 
 ####  Constantes de paramétrage et liste des fichiers a traiter  ####
 cstTestMode = True                                  #True or  False
@@ -53,19 +71,130 @@ scriptProcessing = {
 }
 
 
-####  Options d'appels  ####
+####  Options d'appels pour création des fichiers  ####
 #aArgs = [appName, "-Fall", "-Tall", aixmReader.CONST.optALL, aixmReader.CONST.optIFR, aixmReader.CONST.optVFR, aixmReader.CONST.optFreeFlight, aixmReader.CONST.optCleanLog]
 aArgs = [appName, "-Fall", aixmReader.CONST.typeAIRSPACES, aixmReader.CONST.optALL, aixmReader.CONST.optIFR, aixmReader.CONST.optVFR, aixmReader.CONST.optFreeFlight, aixmReader.CONST.optCleanLog]
 
 
+####  Membres pour globalisation des traitements ####
+globalCatalog = poaffOutPath + referentialPath + globalHeader + "@airspacesCatalog.json"
+oGlobalCatalog:dict = {}
+
+
+def makeNewKey(sKey:str="", oCat:dict={}) -> str:
+    lIdx:int = 1
+    aKey = sKey.split(cstDeduplicateSeparator)
+    if len(aKey)>1: sKey = aKey[0]
+    while True:
+        sNewKey = sKey + cstDeduplicateSeparator + str(lIdx)
+        if not(sNewKey in oCat):
+            break
+        lIdx+=1
+    return sNewKey
+
+def isValidArea(sKeyFile:str, oAs:dict) -> bool:
+    ret:bool = True                     #Default value
+    if oAs["groupZone"]:                #Exclure les zones de regroupement
+        return False
+    
+    if sKeyFile=="BPa-Test4Clean":
+        #Test de suppression manuel d'une zone en mer - [Q] 18 B1 (id=LFD18B1)
+        if oAs["id"] in ["LFD18B1"]: ret = False
+    if sKeyFile=="EuCtrl":
+        #Supprimer les zones
+        #   [D] ALBERT BRAY (CTR / id=LFAQ) - car mauvais tracé et récupération via le fichier SIA
+        #   [R] BRUSSELS FIR (RMZ / EQUIPMENT / id=EBBU RMZ)
+        #   [P] BRUSSELS FIR (TMZ / EQUIPMENT / id=EBBU TMZ)
+        if oAs["id"] in ["LFAQ","EBBU RMZ","EBBU TMZ"]: ret = False
+    #elif sKeyFile=="SIA":
+        # ../..
+    
+    if not(ret):
+        oLog.info("Ignored area by manual filter - ({0}){1}".format(sKeyFile, oAs["id"], outConsole=False))
+    return ret
+
+def mergeCatalogFiles(sKeyFile:str, oFile:dict) -> bool:
+    fileCatalog = oFile[cstOutPath] + referentialPath + sKeyFile + "@airspacesCatalog.json"
+    oLog.info("Consolidation file {0}: {1} -->({2})--> {3}".format(sKeyFile, fileCatalog, oFile[cstProcessType], globalCatalog), outConsole=True)
+    ofileCatalog = bpaTools.readJsonFile(fileCatalog)                                       #Chargement du catalogue du fichier analysé
+    #oLog.info("ofileCatalog:\n{0}".format(ofileCatalog, outConsole=False))
+
+    oHeadFile = ofileCatalog[cstKeyCatalogHeaderFile]                                           #Entête concernant le fichier analysé
+    
+    oGlobalCatalogHeader:dict = {}                                                              #Entête du catalogue gloabal
+    if oGlobalCatalog=={}:                                                                      #Catalogue vde, donc initialisation du catalogue gloabal
+        oGlobalCatalog.update({cstKeyCatalogType:ofileCatalog[cstKeyCatalogType]})              #Typage du catalogue
+        oGlobalCatalogHeader.update({cstKeyCatalogSoftware:oHeadFile[cstKeyCatalogSoftware]})   #Référence au soft de construction
+        oGlobalCatalogHeader.update({cstKeyCatalogCreated:oHeadFile[cstKeyCatalogCreated]})     #Heurodatage de la construction
+        oGlobalCatalogHeader.update({cstKeyCatalogContent:oHeadFile[cstKeyCatalogContent]})     #Déclaration du contenu
+        oGlobalCatalog.update({cstKeyCatalogHeaderFile:oGlobalCatalogHeader})                   #Ajout de l'entête de catalogue
+    else:
+        oGlobalCatalogHeader = oGlobalCatalog[cstKeyCatalogHeaderFile]                          #Entête du catalogue gloabal
+    
+    oCatalogFile:dict = {}                                                                      #Description du fichier analysé
+    oCatalogFile.update({cstKeyCatalogSrcAixmFile:oHeadFile[cstKeyCatalogSrcAixmFile]})         #Nom du fichier analysé
+    oCatalogFile.update({cstKeyCatalogSrcAixmOrigin:oHeadFile[cstKeyCatalogSrcAixmOrigin]})     #Origine du fichier analysé
+    oCatalogFile.update({cstKeyCatalogSrcAixmVersion:oHeadFile[cstKeyCatalogSrcAixmVersion]})   #Version du fichier analysé
+    oCatalogFile.update({cstKeyCatalogSrcAixmCreated:oHeadFile[cstKeyCatalogSrcAixmCreated]})   #Horodatage de la création du fichier analysé
+    
+    if cstKeyCatalogSrcFiles in oGlobalCatalogHeader:
+        oGlobalCatalogFiles = oGlobalCatalogHeader[cstKeyCatalogSrcFiles]                       #Récupération de la liste des fichiers sources
+    else:
+        oGlobalCatalogFiles:dict = {}                                                           #Création de la liste des fichiers sources
+    oGlobalCatalogFiles.update({sKeyFile:oCatalogFile})                                         #Enregistrement de la description du fichier analysé
+    oGlobalCatalogHeader.update({cstKeyCatalogSrcFiles:oGlobalCatalogFiles})                    #Enregistrement de la nouvelle liste des fichiers sources
+    
+    if cstKeyCatalogCatalog in oGlobalCatalog:
+        oGlobalAreas = oGlobalCatalog[cstKeyCatalogCatalog]                                     #Récupération de la liste des zones consolidés
+    else:
+        oGlobalAreas:dict = {}                                                                  #Création d'une liste des zones a consolider
+
+    oAsAreas = ofileCatalog[cstKeyCatalogCatalog]                                             #Catalogue des Espace-aériens contenus dans le fichier analysé
+    for sAsKey, oAs in oAsAreas.items():
+        if isValidArea(sKeyFile, oAs):                                  #Exclure certaines zones
+            oAs.update({cstKeyCatalogOrgFile:sKeyFile})                 #Ajout de la réfénce au fichier source
+            sNewKey = str(oAs["id"]).strip()                            #Nouvelle identifiant de référence pour le catalogue global
+            if sNewKey=="": sNewKey = makeNewKey()                      #Initialisation d'une clé non vide
+            if   oFile[cstProcessType]==cstPtAdd:                       #Ajout systématique des zones (avec débloublonnage des 'id' automatisé)
+                if sNewKey in oGlobalAreas:
+                    sNewKey2 = makeNewKey(sNewKey, oGlobalAreas)        #Identification d'une nouvelle clé unique
+                    oLog.info("Deduplication area for global catalog - orgId={0} --> newId={1}".format(sNewKey, sNewKey2, outConsole=False))
+                    sNewKey = sNewKey2
+                oGlobalAreas.update({sNewKey:oAs})                      #Ajoute la zone au catalogue global
+                oLog.info("Add area in global catalog - ({0}){1}".format(sKeyFile, sNewKey, outConsole=False))
+            elif oFile[cstProcessType]==cstPtAddDelta:                  #Ajout des zones qui ne sont pas déjà existante
+                if sNewKey in oGlobalAreas:                             #Controle prealable de presence
+                    oLog.info("Ignored area (existing in global catalog) - ({0}){1}".format(sKeyFile, sNewKey, outConsole=False))
+                else:
+                    oGlobalAreas.update({sNewKey:oAs})                  #Ajoute la zone au catalogue global
+                    oLog.info("Add area in global catalog - ({0}){1}".format(sKeyFile, sNewKey, outConsole=False))
+            else:
+                oLog.error("Process type error - {0}".format(oFile[cstProcessType], outConsole=True))
+    
+    oGlobalCatalog.update({cstKeyCatalogCatalog:oGlobalAreas})                    #Enregistrement de la nouvelle liste des fichiers sources
+    return
+
+def poaffMergeFiles() -> None:
+    bpaTools.createFolder(poaffOutPath)                             #Initialisation
+    bpaTools.createFolder(poaffOutPath + referentialPath)           #Initialisation
+    bpaTools.deleteFile(globalCatalog)                              #Purge du fichier
+        
+    for sKey, oFile in scriptProcessing.items():                    #Traitement des fichiers
+        if oFile[cstExecute]:                                       #Flag prise en compte du fichier
+            mergeCatalogFiles(sKey, oFile)                                  #Consolidation des fichiers
+
+    #oLog.info("oGlobalCatalog:\n{0}".format(oGlobalCatalog, outConsole=False))
+    bpaTools.writeJsonFile(globalCatalog, oGlobalCatalog)           #Sérialisation du fichier
+    return
+
 def parseFile(sKey:str, oFile:dict) -> bool:
-    bpaTools.createFolder(oFile[cstOutPath])                         #Init dossier de sortie
+    bpaTools.createFolder(oFile[cstOutPath])                        #Init dossier de sortie
     aixmCtrl = aixmReader.AixmControler(oFile[cstSrcFile], oFile[cstOutPath], sKey, oLog)     #Init controler
     ret = aixmCtrl.execParser(oOpts)                                #Execution des traitements
     return ret
 
 def updateReferentials(sKey:str, oFile:dict) -> None:
-    oGEH = GroundEstimatedHeight(oLog, oFile[cstOutPath], oFile[cstOutPath] + "referentials/", sKey + "@")
+    oGEH = GroundEstimatedHeight(oLog, oFile[cstOutPath], oFile[cstOutPath] + referentialPath, sKey + "@")
     oGEH.parseUnknownGroundHeightRef()                              #Execution des traitements
     return
 
@@ -75,14 +204,13 @@ def poaffGenerateFiles(sMsg:str=None) -> None:
     oLog.writeCommandLine(aArgs)                                    #Trace le contexte d'execution
     oLog.info("Loading module - {0}".format(aixmParserId), outConsole=True)
 
-    #### Traitement des fichiers  ####
-    for sKey, oFile in scriptProcessing.items():
-        if oFile[cstExecute]:
-            iPrevErr = oLog.CptCritical + oLog.CptError
-            parseFile(sKey, oFile)
-            iActErr = oLog.CptCritical + oLog.CptError
-            if iActErr!=iPrevErr:
-                updateReferentials(sKey, oFile)
+    for sKey, oFile in scriptProcessing.items():                    #Traitement des fichiers
+        if oFile[cstExecute]:                                       #Flag prise en compte du fichier
+            iPrevErr = oLog.CptCritical + oLog.CptError             #Nombre d'erreurs en pré-traitements
+            parseFile(sKey, oFile)                                  #Creation des fichiers
+            iActErr = oLog.CptCritical + oLog.CptError              #Nombre d'erreurs en post-traitements
+            if iActErr!=iPrevErr:                                   #Si écart constaté
+                updateReferentials(sKey, oFile)                     #Forcer mise à jour des référentiels d'altitudes
     return
 
 def moveFile(sSrcPath:str, sSrcFile:str, sCpyFileName:str, sPoaffWebPageBuffer, sToken:str) -> None:
@@ -139,8 +267,8 @@ def createPoaffWebPage(sHeadFileDate:str=None) -> None:
     fVerionCatalog.write("LastVersion_airspaces-freeflight.geojson;" + sHeadFileDate[:-1] + ";" + sHeadFileDate[:-1] + ";GeoJSON spécifiquement utilisable pour la CFD\n")
 
     # Calalog files
-    sPoaffWebPageBuffer = moveFile(outPath + "EuCtrl/referentials/", "EuCtrl@airspacesCatalog.csv", sHeadFileDate + "airspacesCatalog.csv", sPoaffWebPageBuffer, "@@file@@CSV-airspacesCatalog@@")
-    sPoaffWebPageBuffer = moveFile(outPath + "EuCtrl/referentials/", "EuCtrl@airspacesCatalog.json", sHeadFileDate + "airspacesCatalog.json", sPoaffWebPageBuffer, "@@file@@JSON-airspacesCatalog@@")
+    sPoaffWebPageBuffer = moveFile(outPath + "EuCtrl/" + referentialPath, "EuCtrl@airspacesCatalog.csv", sHeadFileDate + "airspacesCatalog.csv", sPoaffWebPageBuffer, "@@file@@CSV-airspacesCatalog@@")
+    sPoaffWebPageBuffer = moveFile(outPath + "EuCtrl/" + referentialPath, "EuCtrl@airspacesCatalog.json", sHeadFileDate + "airspacesCatalog.json", sPoaffWebPageBuffer, "@@file@@JSON-airspacesCatalog@@")
     fVerionCatalog.write(sHeadFileDate + "airspacesCatalog.csv;" + sHeadFileDate[:-1] + ";" + sHeadFileDate[:-1] + ";Catalogue des espaces-aériens au format CSV\n")
     fVerionCatalog.write(sHeadFileDate + "airspacesCatalog.json;" + sHeadFileDate[:-1] + ";" + sHeadFileDate[:-1] + ";Catalogue des espaces-aériens au format JSON\n")
 
@@ -153,8 +281,8 @@ def createPoaffWebPage(sHeadFileDate:str=None) -> None:
     shutil.copyfile(outPath + "EuCtrl/EuCtrl@airspaces-ifr.geojson", cfdWebPath + "/airspaces-ifr.geojson")
     shutil.copyfile(outPath + "EuCtrl/EuCtrl@airspaces-vfr.geojson", cfdWebPath + "/airspaces-vfr.geojson")
     shutil.copyfile(outPath + "EuCtrl/EuCtrl@airspaces-freeflight.geojson", cfdWebPath + "/airspaces-freeflight.geojson")
-    shutil.copyfile(outPath + "EuCtrl/referentials/EuCtrl@airspacesCatalog.csv", cfdWebPath + "/airspacesCatalog.csv")
-    shutil.copyfile(outPath + "EuCtrl/referentials/EuCtrl@airspacesCatalog.json", cfdWebPath + "/airspacesCatalog.json")
+    shutil.copyfile(outPath + "EuCtrl/" + referentialPath + "EuCtrl@airspacesCatalog.csv", cfdWebPath + "/airspacesCatalog.csv")
+    shutil.copyfile(outPath + "EuCtrl/" + referentialPath + "EuCtrl@airspacesCatalog.json", cfdWebPath + "/airspacesCatalog.json")
 
 
     #### 3/ creating html main page
@@ -181,26 +309,30 @@ def createPoaffWebPage(sHeadFileDate:str=None) -> None:
 if __name__ == '__main__':
     sCallingContext = None
 
-    ####  Préparation d'appel ####
     oOpts = bpaTools.getCommandLineOptions(aArgs)                   #Arguments en dictionnaire
     oLog = bpaTools.Logger(appId, logFile, callingContext, linkContext, isSilent=bool(aixmReader.CONST.optSilent in oOpts))
     if aixmReader.CONST.optCleanLog in oOpts:
         oLog.resetFile()                                            #Clean du log si demandé
 
-    #--------- creation des fichiers et traitements de consolidation ----------
-    poaffGenerateFiles()
+    #--------- creation des fichiers unitaires ----------
+    poaffGenerateFiles()                                            #Creation des fichiers
     if (oLog.CptCritical + oLog.CptError) > 0:
         oLog.resetFile()                                            #Clean du log
         sCallingContext = "Forced reload by second phase"
-        poaffGenerateFiles(sCallingContext)
+        poaffGenerateFiles(sCallingContext)                         #Seconde tentative de creation des fichiers
 
-    #--------- preparation de la mise a jour du site Web ----------
-    if not(cstTestMode):
-        createPoaffWebPage(None)
-        """createPoaffWebPage("20200715_")"""
+    if (oLog.CptCritical + oLog.CptError) > 0:
+        print()
+        oLog.critical("Abort treatment - Show errors in log file", outConsole=True)
+    else:
+        poaffMergeFiles()                                           #Consolidation des fichiers
+        if not(cstTestMode):
+            createPoaffWebPage(None)                                #Preparation pour publication
+            #createPoaffWebPage("20200715_")                        #Pour révision d'une publication
 
     print()
     if sCallingContext!=None:
         oLog.warning("{0}".format(sCallingContext), outConsole=True)
     oLog.Report()
     oLog.closeFile()
+
